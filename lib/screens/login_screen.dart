44ddf064d8e75c488ab6732b55af1ffe1771aca5
Youssefguba <Youssef.guba@abwaab.me>
2023-03-07 22:10:49 +0200

    Assets / TextFormField

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -4,41 +4,77 @@ class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   // build method called 60 time/second
+  // build User Interface / Widgets / Components
+  //
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          color: Colors.red,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.end,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(
-                height: 25,
-                width: 25,
-                color: Colors.green,
-              ),
-              Container(
-                width: 25,
-                height: 25,
-                color: Colors.black,
-              ),
-              Column(
-                children: [
-                  Container(
-                    height: 25,
-                    width: 25,
-                    color: Colors.blue,
-                  ),
-                  Container(
-                    width: 25,
-                    height: 25,
-                    color: Colors.greenAccent,
-                  ),
+              // Image
 
-                ],
+              Image.asset(
+                'assets/images/logo.png',
+                height: 80,
+                width: 80,
               ),
+
+              SizedBox(height: 10),
+              // Text Welcome
+              Text(
+                'Welcome to Lafyuu',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+
+              // empty space
+              SizedBox(
+                height: 10,
+              ),
+
+              // Text Sign In
+              Text(
+                'Sign in to continue',
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+
+              // TextBox Email
+              Container(
+                margin: EdgeInsets.symmetric(
+                  horizontal: 18,
+                  vertical: 16,
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'test@gmail.com',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+
+              // TextBox Password
+
+              // Sign In Button
+
+              // Divider and Text
+
+              // Button Google
+
+              // Button Facebook
+
+              // Text Clickable Forget Password
+
+              // Row [ Text,  Clickable Text ]
             ],
           ),
         ),
