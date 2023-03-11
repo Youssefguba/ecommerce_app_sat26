9c8af6b332fb6d7bf2a9d742e50b930d3e7f85c3
Youssefguba <Youssef.guba@abwaab.me>
2023-03-11 22:18:05 +0200

    Navigation.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app_sat26/screens/register_screen.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -5,10 +6,11 @@ class LoginScreen extends StatelessWidget {
 
   // build method called 60 time/second
   // build User Interface / Widgets / Components
-  //
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: SafeArea(
         child: Container(
           width: MediaQuery.of(context).size.width,
@@ -58,23 +60,117 @@ class LoginScreen extends StatelessWidget {
                   decoration: InputDecoration(
                     hintText: 'test@gmail.com',
                     border: OutlineInputBorder(),
+                    prefixIcon: Icon(
+                      Icons.email_outlined,
+                      color: Colors.blue,
+                    ),
                   ),
                 ),
               ),
 
               // TextBox Password
+              Container(
+                margin: EdgeInsets.symmetric(
+                  horizontal: 18,
+                  vertical: 16,
+                ),
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'enter your password',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(
+                      Icons.password,
+                      color: Colors.blue,
+                    ),
+                    suffixIcon: Icon(
+                      Icons.visibility_off,
+                    ),
+                  ),
+                ),
+              ),
 
               // Sign In Button
+              Container(
+                margin: EdgeInsets.all(10),
+                width: 160,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                  ),
+                  onPressed: () {
+                    print('Sign In Successfully !');
+                  },
+                  child: Text('Sign In'),
+                ),
+              ),
 
               // Divider and Text
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      height: 0.5,
+                      width: MediaQuery.of(context).size.width * 0.25,
+                      color: Colors.black,
+                    ),
+                    Text('OR'),
+                    Container(
+                      height: 0.5,
+                      width: MediaQuery.of(context).size.width * 0.25,
+                      color: Colors.black,
+                    ),
+                  ],
+                ),
+              ),
 
               // Button Google
-
+              OutlinedButton.icon(
+                onPressed: () {},
+                label: Text('Login with Google'),
+                icon: Image.asset(
+                  'assets/images/google_logo.png',
+                  height: 25,
+                ),
+              ),
               // Button Facebook
 
+              OutlinedButton.icon(
+                onPressed: () {},
+                label: Text('Login with Facebook'),
+                icon: Icon(Icons.facebook),
+              ),
               // Text Clickable Forget Password
+              TextButton(
+                onPressed: () {},
+                child: Text('Forget Password?'),
+              ),
 
               // Row [ Text,  Clickable Text ]
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Don't have an account?"),
+                  SizedBox(width: 8),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).pushAndRemoveUntil(
+                          MaterialPageRoute(builder: (context) {
+                        return RegisterScreen();
+                      }), (route) => false);
+                    },
+                    child: Text(
+                      'Register',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
